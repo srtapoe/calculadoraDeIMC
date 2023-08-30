0291ecf46456df87342782fc6d0c038f50ff74c2
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2023-08-29 22:19:04 -0300

    Adicionado métodos iniciais da calculadora de IMC

---
+++ Pessoa.py
@@ -0,0 +1,15 @@
+class Pessoa:
+    def __init__(self):
+        self.nome = input("Digite seu nome: ")
+        self.altura = float(input("Digite sua altura: ").replace(',', '.'))
+        self.peso = float(input("Digite seu peso: ").replace(',', '.'))
+        mostrar_dados(self)
+
+
+def mostrar_dados(self):
+    print(f"Nome: {self.nome}")
+    print(f"Altura: {self.altura}")
+    print(f"Peso: {self.peso}")
+
+
+pessoa1 = Pessoa()
